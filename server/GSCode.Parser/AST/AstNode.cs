0e37840f37b623415fd5574c671a6df762431782
Blakintosh <6821024+Blakintosh@users.noreply.github.com>
2024-11-19 22:21:10 +0000

    Confirmed fix for multi platform language server

--- server/GSCode.Parser/AST/AstNode.cs
+++ server/GSCode.Parser/AST/AstNode.cs
@@ -325,6 +325,8 @@ internal sealed class TernaryExprNode(ExprNode condition, ExprNode? then, ExprNo
 
 internal sealed class IdentifierExprNode(Token identifier) : ExprNode(ExprOperatorType.Operand, identifier.Range)
 {
+    public bool IsAnim { get; } = identifier.Type == TokenType.AnimIdentifier;
+    
     public string Identifier { get; } = identifier.Lexeme;
 }
 
